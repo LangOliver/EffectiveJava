1baa87fb7b8a3eb354f15a9d9a5d9f0fe118343c
Oliver Lang <lang.oliver@gmail.com>
2021-07-03 22:27:11 +0200

    Listen statt Arrays für Typsicherheit

--- src/com/learn/java/Main.java
+++ src/com/learn/java/Main.java
@@ -1,8 +1,26 @@
 package com.learn.java;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        PriorityQueue<Integer> myChoicesQ = new PriorityQueue<>();
+        myChoicesQ.add(2);
+        myChoicesQ.add(3);
+        myChoicesQ.add(22222);
+
+        ArrayList<Integer> myChoices = new ArrayList<>();
+        myChoices.add(20);
+        myChoices.add(30);
+
+	    Chooser<Integer> myChooser = new Chooser<>(myChoices);
+        System.out.println(myChooser.choose());
+
+        Chooser<Integer> myChooser2 = new Chooser<>(myChoicesQ);
+        System.out.println(myChooser2.choose());
+
     }
 }
